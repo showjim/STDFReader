0293ea36d22e316115059098fe468cefbb23fa2b
Jerry Zhou <zhouchao486@gmail.com>
2020-05-27 21:52:32 +0800

    Revert "optimise checkable combo"
    This reverts commit 1cde0729

--- STDF_Reader_GUI.py
+++ STDF_Reader_GUI.py
@@ -595,16 +595,15 @@ class ComboCheckBox(QComboBox):
         self.items.insert(0, 'ALL DATA')
         self.row_num = len(self.items)
         self.Selectedrow_num = 0
-        self.qCheckBox = set() #[]
+        self.qCheckBox = []
         self.qLineEdit = QLineEdit()
         self.qLineEdit.setReadOnly(True)
         self.qListWidget = QListWidget()
         self.addQCheckBox(0)
-        self.qCheckBox(0).stateChanged.connect(self.All)
+        self.qCheckBox[0].stateChanged.connect(self.All)
         for i in range(0, self.row_num):
             self.addQCheckBox(i)
-            self.qCheckBox(i).stateChanged.connect(self.showMessage)
-
+            self.qCheckBox[i].stateChanged.connect(self.showMessage)
         self.setModel(self.qListWidget.model())
         self.setView(self.qListWidget)
         self.setLineEdit(self.qLineEdit)
@@ -616,53 +615,51 @@ class ComboCheckBox(QComboBox):
         self.loadItems(items=self.items[1:])  # 重新添加组件
         for select in select_list:
             index = self.items[:].index(select)
-            self.qCheckBox(index).setChecked(True)  # 选中组件
+            self.qCheckBox[index].setChecked(True)  # 选中组件
         return QComboBox.showPopup(self)
 
+    def printResults(self):
+        list = self.Selectlist()
+        print(list)
+
     def addQCheckBox(self, i):
-        self.qCheckBox.add(QCheckBox()) # append(QCheckBox())
+        self.qCheckBox.append(QCheckBox())
         qItem = QListWidgetItem(self.qListWidget)
-        self.qCheckBox(i).setText(self.items[i])
-        self.qListWidget.setItemWidget(qItem, self.qCheckBox(i))
+        self.qCheckBox[i].setText(self.items[i])
+        self.qListWidget.setItemWidget(qItem, self.qCheckBox[i])
 
     def Selectlist(self):
-        Outputlist = [ch.text() for ch in self.qCheckBox(1:) if ch.isChecked()]
-        # for i in range(1, self.row_num):
-        #     if self.qCheckBox[i].isChecked():
-        #         Outputlist.append(self.qCheckBox[i].text())
+        Outputlist = []
+        for i in range(1, self.row_num):
+            if self.qCheckBox[i].isChecked() == True:
+                Outputlist.append(self.qCheckBox[i].text())
         self.Selectedrow_num = len(Outputlist)
         return Outputlist
 
     def showMessage(self):
+        Outputlist = self.Selectlist()
         self.qLineEdit.setReadOnly(False)
         self.qLineEdit.clear()
-        Outputlist = self.Selectlist()
+        show = ';'.join(Outputlist)
 
         if self.Selectedrow_num == 0:
-            self.qCheckBox[0].setCheckState(0)  # Clear, nothing is selected
-            show = ''
+            self.qCheckBox[0].setCheckState(0)
         elif self.Selectedrow_num == self.row_num - 1:
-            self.qCheckBox[0].setCheckState(2)  # All are selected
-            show = 'ALL DATA'
+            self.qCheckBox[0].setCheckState(2)
         else:
-            self.qCheckBox[0].setCheckState(1)  # Part is/are selected
-            show = ';'.join(Outputlist)
+            self.qCheckBox[0].setCheckState(1)
         self.qLineEdit.setText(show)
         self.qLineEdit.setReadOnly(True)
 
-    def All(self, check_state):
-        if check_state == 2:
-            for ch in self.qCheckBox:
-                ch.setCheckState(2)
-            # for i in range(1, self.row_num):
-            #     self.qCheckBox[i].setChecked(True)
-        elif check_state == 1:
+    def All(self, zhuangtai):
+        if zhuangtai == 2:
+            for i in range(1, self.row_num):
+                self.qCheckBox[i].setChecked(True)
+        elif zhuangtai == 1:
             if self.Selectedrow_num == 0:
                 self.qCheckBox[0].setCheckState(2)
-        elif check_state == 0:
-            # self.clear()
-            for ch in self.qCheckBox:
-                ch.setCheckState(0)
+        elif zhuangtai == 0:
+            self.clear()
 
     def clear(self):
         for i in range(self.row_num):
