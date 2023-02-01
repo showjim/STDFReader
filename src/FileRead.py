3033b24df1af35707f8c426b31be006122116fda
Chao Zhou <chao.zhou@teradyne.com>
2023-02-01 17:11:57 +0800

    fix bug import by last commit

--- src/FileRead.py
+++ src/FileRead.py
@@ -85,7 +85,7 @@ class FileReaders(ABC):
                     # data_summary_all = pd.merge(data_summary_all, data_summary.frame, sort=False, how='outer')
                     data_summary_all = pd.concat([data_summary_all, single_site_df], sort=False,
                                                  join='outer', ignore_index=True)
-                FileReaders.write_to_csv(data_summary_all, output_file_name)
+                # FileReaders.write_to_csv(data_summary_all, output_file_name)
             else:
                 if data_summary_all.empty:
                     data_summary_all = data_summary.frame
