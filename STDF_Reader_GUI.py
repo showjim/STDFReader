2ca659fb4e18df3d9d4d2eaf42570bda2cecdb9d
Chao Zhou <zhouchao486@gmail.com>
2023-11-03 14:23:18 +0800

    close stdf file after convt

--- STDF_Reader_GUI.py
+++ STDF_Reader_GUI.py
@@ -51,7 +51,7 @@ from src.FileRead import FileReaders
 from src.Threads import PdfWriterThread, CsvParseThread, XlsxParseThread, DiagParseThread, SingleRecParseThread
 from llm.chat import ChatBot
 
-Version = 'Beta 0.8.9'
+Version = 'Beta 0.8.10'
 
 
 ###################################################
