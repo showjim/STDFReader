10957b9d2f036615b001744f8cc0f0cfefe32d55
Chao Zhou <zhouchao486@gmail.com>
2024-04-17 17:45:39 +0800

    optimise transpose code

--- test/transpose.py
+++ test/transpose.py
@@ -1,4 +1,8 @@
 import csv
 def transpose():
-    a = zip(*csv.reader(open("test.csv", "rt")))
-    csv.writer(open("transposed.csv", "wt")).writerows(a)
+    a = zip(*csv.reader(open(r".\volt_GPU_site_8_0412.std.gz_csv_log.csv", "rt")))
+    b = list(a)
+    csv.writer(open(r".\volt_GPU_site_8_0412.std.gz_csv_log_transposed.csv", "wt"),lineterminator="\n").writerows(b)
+
+if __name__ == '__main__':
+    transpose()
