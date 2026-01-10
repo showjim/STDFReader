ca46821135cc134eb7d3dab765785d178bdc71ef
Jerry Zhou <zhouchao486@gmail.com>
2026-01-10 17:35:01 +0800

    update version to Beta 0.9.0

--- STDF_Reader_GUI.py
+++ STDF_Reader_GUI.py
@@ -49,7 +49,7 @@ from src.FileRead import FileReaders
 from src.Threads import PdfWriterThread, CsvParseThread, XlsxParseThread, DiagParseThread, SingleRecParseThread
 from llm.chat import ChatBot
 
-Version = 'Beta 0.8.31'
+Version = 'Beta 0.9.0'
 
 
 ###################################################
